fe23385e6c53379051a61aabc49324f5f581221d
David McClure <davidwilliammcclure@gmail.com>
2016-06-06 19:26:24 -0700

    Put markers in a group.

--- website/script.js
+++ website/script.js
@@ -2,18 +2,19 @@
 
 $(function() {
 
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  var map = L.map('map').setView([51.505, -0.09], 2);
 
   var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
 
   map.addLayer(layer)
+
+  var group = L.featureGroup();
 
   // Load the point data.
   $.getJSON('data/80d.geojson', function(json) {
 
     _.each(json.features, function(f) {
 
-      console.log(f.geometry);
       var lat = f.geometry.coordinates[1];
       var lon = f.geometry.coordinates[0];
 
@@ -21,9 +22,11 @@ $(function() {
 
       marker.bindPopup(f.properties.toponym);
 
-      map.addLayer(marker)
+      group.addLayer(marker)
 
     });
+
+    map.addLayer(group);
 
   });
 
