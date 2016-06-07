fd64467829b2ebe776c012cbe5db42b651db5a0c
David McClure <davidwilliammcclure@gmail.com>
2016-06-06 21:02:48 -0700

    Blocking in slider markup.

--- website/script.js
+++ website/script.js
@@ -1,6 +1,8 @@
 
 
-$(function() {
+function createMap(json) {
+
+  // ** Map
 
   var map = L.map('map').setView([51.505, -0.09], 2);
 
@@ -10,24 +12,38 @@ $(function() {
 
   var group = L.featureGroup();
 
-  // Load the point data.
-  $.getJSON('data/80d.geojson', function(json) {
+  // Render the points.
+  _.each(json.features, function(f) {
 
-    _.each(json.features, function(f) {
+    var lat = f.geometry.coordinates[1];
+    var lon = f.geometry.coordinates[0];
 
-      var lat = f.geometry.coordinates[1];
-      var lon = f.geometry.coordinates[0];
-
-      var marker = L.circleMarker([lat, lon]);
-
-      marker.bindPopup(f.properties.toponym);
-
-      group.addLayer(marker)
-
+    var marker = L.circleMarker([lat, lon], {
+      feature: f,
     });
 
-    map.addLayer(group);
+    marker.bindPopup(f.properties.toponym);
+
+    group.addLayer(marker)
 
   });
 
+  map.addLayer(group);
+
+  // ** Slider
+
+  var input = $('input[type="range"]');
+
+  // Set the last offset as the slider max.
+  var maxOffset = _.last(json.features).properties.start;
+  input.attr('end', maxOffset);
+
+  input.rangeslider();
+
+}
+
+
+// Load the point data.
+$(function() {
+  $.getJSON('data/80d.geojson', createMap);
 });
